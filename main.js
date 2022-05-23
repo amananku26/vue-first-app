cf3cb913a19978a11121685fe5f6cdcd4050b2d1
amananku26 <amankumaranku@gmail.com>
2022-05-23 13:43:05 +0530

    ⚡️

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 var app = new Vue({
-    el:'#app',
-    data:{
-        product:'Shocks',
-        image:'assets/img1.jpeg',
-        inStock:false,
-        details:['80% Cotton', '20% Polyster' , 'Mens Wear']
+    el: '#app',
+    data: {
+        product: 'Shocks',
+        image: 'assets/img1.jpeg',
+        inStock: false,
+        details: ['80% Cotton', '20% Polyster', 'Mens Wear'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        }
     }
 })
